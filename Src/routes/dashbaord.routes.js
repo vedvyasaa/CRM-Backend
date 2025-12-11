1a5b6f69b6bf03f1d3575036f83067f47bf3ecc2
VedPrakash Sahu <vedprakashsahu29101999@gmail.com>
2025-12-11 16:03:34 +0530

    Remove old Src folder (case correction)

--- Src/routes/dashbaord.routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/dashboard.routes.js
-import express from 'express';
-import { summary } from '../controllers/dashboard.controller.js';
-import { auth, permit } from '../middleware/auth.middleware.js';
-const router = express.Router();
-
-router.get('/summary', auth, permit('Admin','Staff'), summary);
-
-export default router;
